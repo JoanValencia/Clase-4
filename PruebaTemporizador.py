1bde36beeead37c3215bdfff3e7338fcb5587f56
JoanValencia <52727228+JoanValencia@users.noreply.github.com>
2019-07-15 21:47:32 -0500

    Add files via upload

---
+++ PruebaTemporizador.py
@@ -0,0 +1,73 @@
+
+class Hora(UnidadTiempo):
+
+    def __init__(self)
+        self.hora = v
+        self.htope = 23
+
+
+class Minuto(UnidadTiempo):
+
+    def __init__(self)
+        self.minuto = v
+        self.mtope = 59
+
+        
+class Segundo(UnidadTiempo):
+    
+    def __init__(self)
+        self.segundo = v
+        self.stope = 59
+
+
+class UnidadTiempo(v, t):
+
+    def __init__(self, v, t):
+        self.valor = v
+        self.tope = t
+
+    def iniciar():
+        s = self.segundo.valor()
+        m = self.minuto.valor()
+        h = self.hora.valor()
+
+    def retroceder():
+        if s >= 0:
+            s -= 1
+        elif s < 0:
+            s = t
+            m -= 1
+
+        if m >= 0:
+            m -= 1
+        elif m < 0:
+            m = t
+            h -= 1
+            
+        if h >= 0:
+            h -= 1
+        elif h < 0:
+            h = t
+            s == 0
+
+    def reiniciar():
+        
+
+class Temporizador(UnidadTiempo):
+
+    def __init__(self, h, m, s):
+        self.hora = h
+        self.minuto = m
+        self.segundo = s
+
+    def iniciar(UnidadTiempo[]):
+
+
+    def retroceder():
+
+
+    def reiniciar():
+
+
+
+    
